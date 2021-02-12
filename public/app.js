0db3b30ed81df16a9b76c68bd86f33822c02586d
rabbygit <rabbyhossain482@gmail.com>
2021-02-12 11:06:34 +0600

    server configured to serve static file

---
+++ public/app.js
@@ -0,0 +1,16 @@
+/*
+ * Javascript logic for front end
+ *
+ */
+
+// Container for frontend application
+const app = {}
+
+// Config
+app.config = {
+    'sessionToken': false
+}
+
+
+
+console.log("Hello world console log")
